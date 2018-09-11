235a6dffa9928589ff48c1450d406e63e6623b91
rishichawda <chawda.rishii@gmail.com>
2018-09-11 22:11:20 +0530

    feat(src/components): add avatar component into the set

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ export { default as Button } from './components/Button';
 export { default as Image } from './components/Image';
 export { default as Row } from './components/Row';
 export { default as Card } from './components/Card';
+export { default as Avatar } from './components/Avatar';
