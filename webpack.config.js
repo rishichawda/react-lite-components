57bd2d46e0874e7d62e741f1caedf71cdf443d2d
rishichawda <chawda.rishii@gmail.com>
2019-01-20 01:32:43 +0530

    build(webpack.config): update build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,58 @@
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const path = require('path')
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve('lib'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2',
+// Change this to get source maps while development.
+const devtool = ''
+
+// Module entry point.
+const entryPoint = './src/index.js'
+
+// Output path and targets.
+const output = {
+  path: path.resolve('lib'),
+  filename: 'index.js',
+  libraryTarget: 'commonjs2',
+}
+
+// Exclude react, react-dom and styled-components from the bundle.
+const externalModules = [
+  'react',
+  'react-dom',
+  'styled-components'
+]
+
+// Configure status logs while build.
+const statsConfig = {    
+  colors: true,
+  hash: true,
+  timings: true,
+  assets: true,
+  chunks: true,
+  chunkModules: true,
+  modules: true,
+  children: true,
+}
+
+// Splitchunks configuration.
+const splitChunks = {
+  cacheGroups: {
+    default: false,
+    styles: {
+      name: 'styles',
+      test: /\.css$/,
+      chunks: 'all',
+      enforce: true,
+    },
+    commons: {
+      test: /[\\/]node_modules[\\/]/,
+      name: 'vendor_app',
+      chunks: 'all',
+      minChunks: 2,
+    },
   },
+}
+
+module.exports = {
   module: {
     rules: [
       {
@@ -39,17 +83,15 @@ module.exports = {
       },
     ],
   },
-  stats: {    
-    colors: true,
-    hash: true,
-    timings: true,
-    assets: true,
-    chunks: true,
-    chunkModules: true,
-    modules: true,
-    children: true,
-  },
+  devtool,
+  mode: 'production',
+  entry: entryPoint,
+  output,
+  externals: externalModules,
+  stats: statsConfig,
   optimization: {
+    splitChunks,
+    runtimeChunk: false,
     minimizer: [
       new UglifyJSPlugin({
         sourceMap: true,
@@ -60,23 +102,5 @@ module.exports = {
         },
       }),
     ],
-    runtimeChunk: false,
-    splitChunks: {
-      cacheGroups: {
-        default: false,
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true,
-        },
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor_app',
-          chunks: 'all',
-          minChunks: 2,
-        },
-      },
-    },
   }
 }
