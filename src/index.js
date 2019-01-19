6e9ad4eab8a4d090997540500825ab65ff953b1b
rishichawda <chawda.rishii@gmail.com>
2019-01-19 19:37:12 +0530

    chore(src): remove old code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 // Basic Components
-export { default as Container } from './components/Container';
-export { default as Input } from './components/Input';
-export { default as Button } from './components/Button';
-export { default as Image } from './components/Image';
-export { default as Row } from './components/Row';
-export { default as Card } from './components/Card';
-export { default as Avatar } from './components/Avatar';
-export { default as Col } from './components/Column';
-export { default as Badge } from './components/Badge';
+export { default as Text } from './components/text';
